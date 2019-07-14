f00bda0e2b50288ab9b5ca764841d99d7a2355d7
Malwina Leśniewska <malwina.lesniewska@gmail.com>
2019-07-14 19:06:05 +0200

    reorganized folders, added scss files instead of css; added Header component to test if everything works

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './_App.scss';
+import Header from '../Header/Header';
+
+function App() {
+  return (
+    <Header></Header>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './_Header.scss';
+
+class Header extends Component {
+
+    render () {
+        return (
+            <div className="Header">
+                <header>
+                    Hello World!
+                </header>
+            </div>
+        )
+    }
+}
+
+export default Header;
